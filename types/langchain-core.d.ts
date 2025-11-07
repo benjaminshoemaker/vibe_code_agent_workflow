81f0523dee1d90bf0ba3a08a64a89d5f1f0f8f80
Benjamin Shoemaker <ben.shoemaker.xyz@gmail.com>
2025-11-07 13:38:24 -0800

    fixing tests

---
+++ types/langchain-core.d.ts
@@ -0,0 +1,4 @@
+declare module "@langchain/core/runnables" {
+  export * from "@langchain/core/dist/runnables/index.js";
+  export type { RunnableConfig } from "@langchain/core/dist/runnables/types.js";
+}
